6290965c1a5e342519d065e20bb868371769ee96
Lam Thanh Trung <trunglt251292@gmail.com>
2019-07-25 10:49:33 +0700

    server

--- Bot-telegram/index.js
+++ Bot-telegram/index.js
@@ -10,33 +10,33 @@ export default class Bot {
     this.bot.onText(/\/start/, async (msg) => {
       try{
         let count_airdrop = await User.count({});
-        if(count_airdrop < Config.airdrop_limit){
-          let inviteCode = msg.text.split(' ')[1];
-          if (inviteCode){
-            let checkuser = await User.findOne({telegram_id: inviteCode});
-            if (checkuser && parseInt(inviteCode) !== checkuser.telegram_id){
-              let userUseCode = await User.findOne({telegram_id: msg.from.id});
-              if(!userUseCode){
-                await User.create({
-                  telegram_id: msg.from.id,
-                  invited_by: inviteCode
-                });
-                if(checkuser.ref_count < 3){
-                  checkuser.balance += Config.ref_bonus;
-                  checkuser.ref_count = await User.count({invited_by: inviteCode});
-                  await checkuser.save();
-                }
+        // if(count_airdrop < Config.airdrop_limit){
+        let inviteCode = msg.text.split(' ')[1];
+        if (inviteCode){
+          let checkuser = await User.findOne({telegram_id: inviteCode});
+          if (checkuser && parseInt(inviteCode) !== checkuser.telegram_id){
+            let userUseCode = await User.findOne({telegram_id: msg.from.id});
+            if(!userUseCode){
+              await User.create({
+                telegram_id: msg.from.id,
+                invited_by: inviteCode
+              });
+              if(checkuser.ref_count < 3){
+                checkuser.balance += Config.ref_bonus;
+                checkuser.ref_count = await User.count({invited_by: inviteCode});
+                await checkuser.save();
               }
             }
           }
-          this.bot.sendPhoto(msg.chat.id, 'https://storage.googleapis.com/skull/image/banner.jpg');
-          this.bot.sendMessage(msg.chat.id,Config.HelloMessage, Config.buttons);
-        } else {
-          this.bot.sendPhoto(msg.chat.id, 'https://storage.googleapis.com/skull/image/banner.jpg')
-          this.bot.sendMessage(msg.chat.id,"PO8 Token Airdrop have finished.");
         }
+        this.bot.sendPhoto(msg.chat.id, 'https://storage.googleapis.com/skull/image/banner.jpg');
+        this.bot.sendMessage(msg.chat.id,Config.HelloMessage, Config.buttons);
+        // } else {
+        //   this.bot.sendPhoto(msg.chat.id, 'https://storage.googleapis.com/skull/image/banner.jpg')
+        //   this.bot.sendMessage(msg.chat.id,"PO8 Token Airdrop have finished.");
+        // }
       }catch (e) {
-        this.bot.sendMessage(msg.chat.id,'Error connect server bankcex.');
+        this.bot.sendMessage(msg.chat.id,'Error connect server.');
       }
     });
 
